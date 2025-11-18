c0af8f515715a734a11ac9b1e288e68b9590126a
AndreyOlsh <andrey.olsh@rambler.ru>
2025-11-18 13:52:38 +0300

    Create Hw_1_WelcomePHP.php

---
+++ Hw_1_WelcomePHP.php
@@ -0,0 +1,17 @@
+<?php
+    /*1. VS Code разделить окно на два:
+    Горизонтально: Ctrl + \
+    Вертикально: Ctrl + Shift + \
+    Или: правой кнопкой по вкладке → Split Right/Left/Down/Up
+    Перемещение: Ctrl + 1, Ctrl + 2 
+    http://localhost/HWorks_PHP/Hw_1_WelcomePHP.php
+    */
+?>
+<?php
+
+    for ($i = 1; $i < 3; $i++)
+    {
+        echo "Квадрат числа $i равен " . $i * $i . "<br/>";
+    }
+
+?><br>
