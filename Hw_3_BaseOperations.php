257a23c43a4b085ade2ad2994c701498adced57f
AndreyOlsh <andrey.olsh@rambler.ru>
2025-11-25 13:12:59 +0300

    Create Hw_3_BaseOperations.php

---
+++ Hw_3_BaseOperations.php
@@ -0,0 +1,7 @@
+<?php
+    /*Домашнее задание к занятию «1.1. Знакомство с PHP»
+    http://localhost/HWorks_PHP/Hw_3_BaseOperations.php 
+    для консоли
+    1.  cd C:\localhost\HWorks_PHP 
+    2. php Hw_3_BaseOperations.php*/
+?>
