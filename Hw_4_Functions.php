95ca3f4fb3b2b2408f26b3e5e7e85a19021719ab
AndreyOlsh <andrey.olsh@rambler.ru>
2025-11-28 11:54:11 +0300

    Create Hw_4_Functions.php

---
+++ Hw_4_Functions.php
@@ -0,0 +1,11 @@
+<?php
+    /*Домашнее задание к занятию «1.1. Знакомство с PHP»
+    http://localhost/HWorks_PHP/Hw_4_Functions.php 
+    для консоли
+    1.  cd C:\localhost\HWorks_PHP 
+    2. php Hw_4_Functions.php
+    */
+?>
+<?php
+
+?>
