b8b14fe5ae04fb9ede981c57bfb5ac22ae93dae2
AndreyOlsh <andrey.olsh@rambler.ru>
2025-12-12 12:06:29 +0300

    Create Hw_7_Forms.php

---
+++ Hw_7_Forms.php
@@ -0,0 +1,8 @@
+<?php
+    /*Домашнее задание к занятию «7_Файлы, html формы»
+    http://localhost/HWorks_PHP/Hw_7_Forms.php 
+    для консоли
+    1. cd C:\localhost\HWorks_PHP 
+    2. php Hw_7_Forms.php
+    */
+?>
